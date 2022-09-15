87f91576564aee88cb90354b172d701466fb0457
Zeynel Şahin <zynl.shn@hotmail.com>
2022-09-15 11:26:27 +0300

    Initial commit

---
+++ DotNet6-Json/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+
+var product = new Product() { Name = "Zeynel", Age = 21, Surname = "Sahin" };
+FileStream createStream = File.Create("ProductAsync.json");
+string jsonString = JsonSerializer.Serialize(product);
+await JsonSerializer.SerializeAsync(createStream, product);
+
+Console.WriteLine(jsonString);
+
+string jsonStringGenerics = JsonSerializer.Serialize<Product>(product);
+Console.WriteLine(jsonStringGenerics);
+
+string fileName = "Product.json";
+File.WriteAllText(fileName, jsonString);
+
+class Product
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public short Age { get; set; }
+}
